c29dd3e526b2bfdc4f95e2841a6f73de59594668
Stefan Bechert <stefan.bechert.inf@gmail.com>
2019-07-01 14:21:40 +0200

    [incomplete-feature] Add code for dragging a scroll view that does sometimes work and somtimes not!?

---
+++ Assets/_scripts/_custom_ui/GetavizScrollRect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+class GetavizScrollRect : ScrollRect
+{
+    private readonly float animateChangePerFrame = 0.05f;
+    private float verticalNormalizedTargetPosition = 1f;
+
+    private bool coroutineIsRunning = false;
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        if (!this.coroutineIsRunning)
+        {
+            this.StartCoroutine(this.AnimateScroll());
+        }
+
+        this.verticalNormalizedTargetPosition -= eventData.scrollDelta.y;
+        this.verticalNormalizedTargetPosition = Mathf.Clamp01(this.verticalNormalizedTargetPosition);
+    }
+
+    private IEnumerator AnimateScroll()
+    {
+        this.coroutineIsRunning = true;
+
+        while (true)
+        {
+            if ((Mathf.Abs(this.verticalNormalizedTargetPosition - this.verticalNormalizedPosition) > this.animateChangePerFrame))
+            {
+                Canvas.ForceUpdateCanvases();
+                if (this.verticalNormalizedTargetPosition > this.verticalNormalizedPosition)
+                {
+                    this.verticalNormalizedPosition += this.animateChangePerFrame;
+                }
+                else
+                {
+                    this.verticalNormalizedPosition -= this.animateChangePerFrame;
+                }
+            } 
+            yield return null;
+        }
+    }
+}
